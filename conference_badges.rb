06d0646bb0d55aabc54fa5c476e84ba8508fb020
Franklyn Rodriguez <franklynr92@gmail.com>
2020-04-16 14:51:09 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,7 +1,8 @@
 # Write your code here.
     def badge_maker(name)
-     puts "Hello, my name is #{name}."
      name = ["Arel"]
+     puts "Hello, my name is #{name}."
+     
 end
 # def batch_badge_creator
 
