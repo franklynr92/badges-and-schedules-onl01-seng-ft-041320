da18d9ccebb2a06f719f2f3d97947e189b5550ed
Franklyn Rodriguez <franklynr92@gmail.com>
2020-04-16 15:38:04 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,7 @@ def badge_maker(name)
  "Hello, my name is Arel."
 end
 
-def badge_maker_creator
+def badge_maker_creator(name)
  names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
  names.each {|name|  "Hello, my name is #{name}."}
 end
