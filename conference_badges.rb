f7646bac61891afccce4f1822fe875d408d38169
Franklyn Rodriguez <franklynr92@gmail.com>
2020-04-16 15:43:13 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,5 +5,5 @@ end
 
 def batch_badge_creator(attendees)
  badges = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
- names.each {|attendees| puts "Hello, my name is #{attendees}."}
+ badges.each {|attendees| puts "Hello, my name is #{attendees}."}
 end
